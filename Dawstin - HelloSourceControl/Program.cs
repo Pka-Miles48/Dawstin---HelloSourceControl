f7474b16dc8df0bc1bf2abd1931b9d3141f45411
Pka-Miles48 <151704300+Pka-Miles48@users.noreply.github.com>
2024-07-06 03:38:30 -0700

    Update Program.cs

--- Dawstin - HelloSourceControl/Program.cs
+++ Dawstin - HelloSourceControl/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hi, GitHub!");
+
+for  (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Number: {i}");
+}
